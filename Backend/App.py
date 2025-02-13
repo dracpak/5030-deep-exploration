ce7da2546098c23e6ddbdfc6cd8f453736831c10
unknown <spkann7@gmail.com>
2025-02-13 10:21:29 -0600

    Backend Functionality

---
+++ Backend/App.py
@@ -0,0 +1,23 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+from DBConnector import DBConnector
+
+app = Flask(__name__)
+CORS(app, origins=['http://localhost:3000', 'http://127.0.0.1:3000', '*'])
+
+db = DBConnector()
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	user = request.get_json()
+	data = db.login(user['username'], user['password'])
+	return jsonify(data), 200
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+	newUser = request.get_json()
+	data = db.register(newUser)
+	return jsonify(data), 201
+
+if __name__ == '__main__':
+    app.run()
---
+++ Backend/DBConnector.py
@@ -0,0 +1,56 @@
+import psycopg2
+import psycopg2.extras
+import hashlib
+import secrets
+
+class DBConnector:
+   
+	def __init__(self):
+		try:
+			# connecting to the PostgreSQL server
+			self.connection = psycopg2.connect('insert credentials here')
+			print('Connected to the PostgreSQL server.')
+
+		except (psycopg2.DatabaseError, Exception) as error:
+			print(error)
+            
+	def login(self, username, password):
+		try:
+			with self.connection.cursor() as cur:
+				sql = f'SELECT password FROM users WHERE username=\'{username}\''
+				cur.execute(sql)
+				result = cur.fetchone()[0]
+		except Exception:
+			self.connection.rollback()
+			result = None
+		finally:
+			if cur:
+				cur.close()
+				
+		salt = result[:4]
+		passwordHash = result[4:]
+		if (hashlib.sha256((salt+password).encode()).hexdigest() == passwordHash):
+			return {'result': 'success'}
+		else:
+			return {'result': 'fail'}
+			
+	def register(self, newUser):
+		
+		username = newUser['username']
+		email = newUser['email']
+		salt = secrets.token_hex(2)
+		password = salt+hashlib.sha256((salt+newUser['password']).encode()).hexdigest()
+		try:
+			with self.connection.cursor() as cur:
+				sql = f'''
+				INSERT INTO users (username, password, email) 
+				VALUES ('{username}', '{password}', '{email}');
+				'''
+				cur.execute(sql)
+				self.connection.commit()
+		except Exception:
+			self.connection.rollback()
+		finally:
+			if cur:
+				cur.close()
+		return {'result': 'success'}
