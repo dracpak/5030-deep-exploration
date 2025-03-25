de9f1bb9b5c956e0c7525c1434970aa746d29749
DavydakoroS <04davidsv@gmail.com>
2025-03-25 14:55:28 -0500

    Initial commit

--- Backend/app.py
+++ Backend/app.py
@@ -1,32 +1,49 @@
 """
 Main file for backend, run to set up server
 """
+from bot import process_user_choice 
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-from db_connector import DBConnector
+#from db_connector import DBConnector
 
 app = Flask(__name__)
 CORS(app, origins=['http://localhost:3000', 'http://127.0.0.1:3000', '*'])
 
-db = DBConnector()
+#db = DBConnector()
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    """
-    Check login credentials of user
-    """
-    user = request.get_json()
-    data = db.login(user['username'], user['password'])
-    return jsonify(data), 200
+#@app.route('/login', methods=['GET', 'POST'])
+#def login():
+#    """
+#    Check login credentials of user
+#    """
+#    user = request.get_json()
+#    data = db.login(user['username'], user['password'])
+#    return jsonify(data), 200
 
-@app.route('/register', methods=['GET', 'POST'])
-def register():
+#@app.route('/register', methods=['GET', 'POST'])
+#def register():
+#    """
+#    Register a new user
+#    """
+#    new_user = request.get_json()
+#    data = db.register(new_user)
+#    return jsonify(data), 201
+
+
+@app.route('/chat', methods=['POST'])
+def chat():
     """
-    Register a new user
+    Process user choice and return next step in chat
     """
-    new_user = request.get_json()
-    data = db.register(new_user)
-    return jsonify(data), 201
+    data = request.get_json()
+    step_id = data.get('step_id', 'start')
+    user_choice_index = data.get('choice_index')
+
+    next_step = process_user_choice(step_id, user_choice_index)
+
+    return jsonify(next_step), 200
+
+
 
 if __name__ == '__main__':
     app.run()
