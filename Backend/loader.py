06ebd5e62c479b6e57efd67372cf697493376a72
dracpak <spkann7@gmail.com>
2025-03-26 16:35:27 -0500

    Linting pt 2

--- Backend/loader.py
+++ Backend/loader.py
@@ -23,7 +23,7 @@ def load_json(filepath):
     .json loader function
     """
     try:
-        with open(filepath, 'rt') as file:
+        with open(filepath, 'rt', encoding='utf-8') as file:
             data = json.load(file)
             return data
     except FileNotFoundError:
