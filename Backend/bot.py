c082561a18d2a1be4ebec900e12cfca048b35799
DavydakoroS <04davidsv@gmail.com>
2025-04-21 00:52:43 -0500

    Add multi-dialog support (dialog1, dialog2, dialog3)

--- Backend/bot.py
+++ Backend/bot.py
@@ -5,20 +5,21 @@ from loader import load_json
 
 chat_flow = load_json('chat_flow.json')
 
-def get_chat_step(step_id):
+
+def get_chat_step(dialog_id: str, step_id: str):
     """
     Gets the text of the current chat
     """
-    return chat_flow.get(step_id, chat_flow["start"])
+    flow = chat_flow.get(dialog_id) or chat_flow['dialog1']
+    return flow.get(step_id) or flow['start']
 
-def process_user_choice(current_step, user_choice_index):
+def process_user_choice(dialog_id: str, current_step: str, user_choice_index: int):
     """
     Returns the next step in the chat based on the user's choice
     """
-    current_data = get_chat_step(current_step)
-
-    if user_choice_index < 0 or user_choice_index >= len(current_data['options']):
-        return current_data
-
-    next_step_id = current_data['options'][user_choice_index]['next']
-    return get_chat_step(next_step_id)
+    current = get_chat_step(dialog_id, current_step)
+    options = current.get('options', [])
+    if not (0 <= user_choice_index < len(options)):
+        return current  # Invalid choice,return current step
+    next_id = options[user_choice_index]['next']
+    return get_chat_step(dialog_id, next_id)
